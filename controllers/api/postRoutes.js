9dc5b8a034c1f24a0bfb6fd4ab4b4a3e41fa11b7
Youssef <youssefkaroui6@gmail.com>
2023-10-04 11:07:00 -0400

    postRoutes added

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const existingPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+        userId: req.session.userId,
+      },
+    });
+
+    if (!existingPost) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(existingPost);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
