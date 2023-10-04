19125c8bdaa065a00c81f6ee72d601adf25bdb4d
Youssef <youssefkaroui6@gmail.com>
2023-10-04 16:03:45 -0400

    added logic to add a new post and delete a post to post.js

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+const logoutHandler = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    const results = await response.json();
+    console.log(results);
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logoutHandler);
---
+++ public/js/post.js
@@ -0,0 +1,50 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const PostTitle = document.querySelector('#post-title').value.trim();
+    const postBody = document.querySelector('#post-body').value.trim();
+  
+    if (PostTitle && postBody) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, postBody }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to post');
+      }
+    }
+  };
+  
+  const deletePostHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+  
+  let newPost = document
+    .querySelector('.new-post-form')
+    if (newPost){
+      newPost.addEventListener('submit', newFormHandler);
+    }
+  
+  let postList = document
+    .querySelector('.post-list')
+    if (postList){
+      postList.addEventListener('click', delButtonHandler);
+    };
