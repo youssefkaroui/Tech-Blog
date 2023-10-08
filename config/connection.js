f2d620974313cbd31a3c86ffef0c6b95157df1af
Youssef <youssefkaroui6@gmail.com>
2023-10-08 13:15:57 -0400

    fixed the passwird variable name in the connections.js; now its launching properly

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 let sequelize;
 
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
@@ -17,4 +18,4 @@ if (process.env.JAWSDB_URL) {
     },
   );
 }
-module.exports = sequelize;
+module.exports =  sequelize;
